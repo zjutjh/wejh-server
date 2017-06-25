6b8b257fd9bac660adf75ced0ddde31777592dbe
陈然 <chen.raim@qq.com>
2017-06-25 22:27:24 +0800

    User Model

---
+++ app/Models/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Model;
+use Moloquent;
+
+class User extends Moloquent
+{
+    // mongodb的易扩展特性导致某些限制只能在PHP里做出配置，所以务必在所有Model里做出一些相关配置
+
+    // 此处定义create时的默认值，请务必配置，以便保证字段完整性，因为返回给前端时是不走访问器的，所以需要设置字段减少前端工作
+    protected $attributes = array(
+        // 'password' => '',
+        'avatar' => null, // 头像url
+        'nickname' => null,// 微信昵称 or else
+        'subscribe' => 0, // 是否关注公众号
+        'name' => null, // 真实姓名
+        'gender' => 0, //性别
+        'grade' => null, // 年级
+        'college' => null, // 学院
+        'major' => null, // 专业
+        'class' => null, // 班级
+        'user_group' => 0, // 用户组, 默认普通用户
+        'remember_token' => null, // 登录token标识
+        // 接下来是一些用户配置
+        'score_term' => '2017/2018(1)', // 成绩学期
+        'class_term' => '2017/2018(1)', // 课表学期
+        'exam_term' => '2017/2018(1)', // 排考学期
+        'jh_password' => null, // 精弘通行证密码
+        'yc_password' => null, // 原创教务密码
+        'card_password' => null, // 一卡通密码
+        'lib_password' => null, // 图书馆密码
+
+    );
+
+    protected $primaryKey = '_id';
+    public $incrementing = false;
+
+    /**
+     * 获取用户的名字
+     *
+     * @param  string  $value
+     * @return string
+     */
+    public function getNameAttribute($value)
+    {
+        return $value ? $value : '';
+    }
+}
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,13 +13,26 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
+        // 由于mongodb的特性，设置非主键的字段无意义，所以在此将一些非主键字段注释掉，反正可以瞎几把扩展
         Schema::create('users', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+            // $table->increments('uid')->comment("自增长用户id");
+            $table->string('uno')->unique()->comment("学号");
+            // $table->string('password')->nullable()->comment("密码");
+            $table->string('email', 100)->unique()->nullable()->comment("邮箱");
+            $table->string('phone', 100)->unique()->nullable()->comment("手机");
+            // $table->string('avatar')->default('')->comment("微信头像");
+            // $table->string('nickname')->default('')->comment("微信昵称");
+            // $table->integer('subscribe')->default(0)->comment("是否关注");
+            // $table->string('name')->default('')->comment("姓名");
+            // $table->tinyInteger('sex')->default(0)->comment("性别0-女 1-男");
+            // $table->string('grade')->default('')->comment("年级");
+            // $table->string('college')->default('')->comment("学院");
+            // $table->string('major')->default('')->comment("专业");
+            // $table->string('class')->default('')->comment("班级");
+            // $table->text('user_setting')->comment("用户设置");
+            // $table->tinyInteger('is_admin')->default(0)->comment("是否管理员");
+            // $table->rememberToken()->comment("登录token");
+            // $table->timestamps();
         });
     }
 
