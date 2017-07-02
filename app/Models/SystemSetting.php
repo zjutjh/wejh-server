76583de48ec77ed68fca0fea05ee461b0f15100c
陈然 <chen.raim@qq.com>
2017-07-03 00:29:14 +0800

    add SystemSetting

---
+++ app/Models/SystemSetting.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: cccRaim
+ * Date: 2017/7/3
+ * Time: 00:11
+ */
+
+namespace App\Models;
+
+// use Illuminate\Database\Eloquent\Model;
+use Moloquent;
+
+class SystemSetting extends Moloquent
+{
+    // 此处定义默认值，要保证每个值都有对应的访问器
+    protected $defaults = array(
+        // 'varname'        // 名称
+        // 'value'          // 值
+    );
+
+    protected $hidden = [
+        '_id',
+        'updated_at',
+        'created_at'
+    ];
+
+    // 后期另外增加的字段,不需定义，只需在上面默认值处增加字段
+    protected $appends;
+
+    protected $primaryKey = '_id';
+    public $incrementing = false;
+
+    function __construct()
+    {
+        $this->appends = array_keys($this->defaults);
+        parent::__construct(...func_get_args());
+    }
+
+    /**
+     * 不能被批量赋值的属性
+     *
+     * @var array
+     */
+    protected $guarded = ['_id'];
+
+    /**
+     * 返回collection的值，如不存在，则返回默认值
+     * @param  string   $field
+     * @return mixed
+     */
+    public function getValue($field) {
+        if (isset($this->attributes[$field])) {
+            return $this->attributes[$field];
+        } else {
+            return $this->defaults[$field];
+        }
+    }
+}
---
+++ database/migrations/2017_07_02_160337_create_system_settings_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSystemSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('system_settings', function (Blueprint $table) {
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('system_settings');
+    }
+}
