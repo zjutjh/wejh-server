03d4a0bf1e26f42a893dcab8473480e970bc5219
cccRaim <chen.raim@qq.com>
2017-07-14 23:33:33 +0800

    增加登录控制器

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,36 +4,36 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Auth;
+use JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
 
 class LoginController extends Controller
 {
-    /*
-    |--------------------------------------------------------------------------
-    | Login Controller
-    |--------------------------------------------------------------------------
-    |
-    | This controller handles authenticating users for the application and
-    | redirecting them to your home screen. The controller uses a trait
-    | to conveniently provide its functionality to your applications.
-    |
-    */
-
-    use AuthenticatesUsers;
-
     /**
-     * Where to redirect users after login.
-     *
-     * @var string
+     * 认证用户，返回token
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    protected $redirectTo = '/home';
-
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public function authenticate(Request $request)
     {
-        $this->middleware('guest')->except('logout');
+        // grab credentials from the request
+        $credentials = $request->only('uno', 'password');
+
+        try {
+            // attempt to verify the credentials and create a token for the user
+            if (! $token = JWTAuth::attempt($credentials)) {
+                return RJM(null, -401, '用户名或密码错误');
+            }
+        } catch (JWTException $e) {
+            // something went wrong whilst attempting to encode the token
+            return RJM(null, -500, 'token生成错误');
+        }
+
+        // all good so return the token
+        return RJM([
+            'token' => $token
+        ], 200, '登陆成功');
     }
 }
