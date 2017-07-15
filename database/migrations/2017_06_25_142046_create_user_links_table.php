5ac8f1993b2e5787280a379b358e37d38e3900cd
cccRaim <chen.raim@qq.com>
2017-07-15 22:48:17 +0800

    user link中改为uid

--- database/migrations/2017_06_25_142046_create_user_links_table.php
+++ database/migrations/2017_06_25_142046_create_user_links_table.php
@@ -15,7 +15,7 @@ class CreateUserLinksTable extends Migration
     {
         Schema::create('user_links', function (Blueprint $table) {
             $table->increments('id')->comment("自增长id");
-            $table->string('uno')->comment("关联用户学号or教职工号");
+            $table->string('uid')->comment("关联用户id");
             $table->string('type')->comment("第三方平台类型");
             $table->string('openid')->comment("第三方平台用户标识");
             $table->string('access_token')->comment("第三方平台用户授权凭证");
