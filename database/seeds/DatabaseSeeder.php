b9290b9c5dca7563443469714c7b816c25b8bd2e
cccRaim <chen.raim@qq.com>
2017-07-16 01:36:46 +0800

    增加系统设置导入

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(UserTableSeeder::class);
         $this->call(UserGroupTableSeeder::class);
+        $this->call(SystemSettingTableSeeder::class);
     }
 }
