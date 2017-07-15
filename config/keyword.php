9afbc8463c5921f8d9f05204db29d41e876eb086
cccRaim <chen.raim@qq.com>
2017-07-15 14:47:18 +0800

    增加关键词配置

---
+++ config/keyword.php
@@ -0,0 +1,43 @@
+<?php
+
+
+return [
+    [
+        'content' => '查成绩',
+        'type' => 1,//1为等于，2为包含，3为正则
+        'reply' => [
+            'type' => 'function',
+            'content' => 'score'
+        ],
+        'status' => 1,//是否禁用，0为禁用
+    ],
+    [
+        'content' => '查饭卡',
+        'type' => 1,//1为等于，2为包含，3为正则
+        'reply' => [
+            'type' => 'function',
+            'content' => 'card'
+        ],
+        'status' => 1,//是否禁用，0为禁用
+    ],
+    [
+        'content' => '查排考',
+        'type' => 1,//1为等于，2为包含，3为正则
+        'reply' => [
+            'type' => 'function',
+            'content' => 'exam'
+        ],
+        'status' => 1,//是否禁用，0为禁用
+    ],
+    [
+        'content' => '绑定',
+        'type' => 1,//1为等于，2为包含，3为正则
+        'reply' => [
+            'type' => 'news',
+            'title' => '绑定',
+            'description' => '点击此处绑定',
+            'url' => url('/login'),
+        ],
+        'status' => 1,//是否禁用，0为禁用
+    ],
+];
