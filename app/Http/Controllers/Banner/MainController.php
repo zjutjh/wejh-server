5a5eb1373ac8a4791a7d71e8559b2a720ab9b1bd
cccRaim <chen.raim@qq.com>
2017-07-16 22:29:55 +0800

    banner以及tip逻辑

---
+++ app/Http/Controllers/Banner/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers\Banner;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MainController extends Controller
+{
+    public function banner(Request $request) {
+        $link = 'https://wejh.craim.net/weixincard/MzI0MTUzNzQzMg';
+        $img = url('/img/banner/0.png');
+        $res = [
+            'show' => true,
+            'router' => false,
+            'img' => $img,
+            'title' => '精弘网络团队',
+            'link' => $link,
+            'linkDescribe' => '查看更多'
+        ];
+        return RJM($res, 1, 'ok');
+    }
+}
---
+++ app/Http/Controllers/Tip/MainController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Tip;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class MainController extends Controller
+{
+    public function tip(Request $request) {
+        $message = '精弘十五周年，感谢有你（面向朝晖）';
+        $link = 'http://mp.weixin.qq.com/s/ntWojtX_TTChHndBWKb2Zw';
+        $res = [
+            'msg' => $message,
+            'link' => $link
+        ];
+        return RJM($res, 1, 'ok');
+    }
+}
