623e673f9499e16b5099cc30e2aef9032eab1a9f
cccRaim <chen.raim@qq.com>
2017-09-10 01:19:41 +0800

    增加图标的map

--- app/Http/Controllers/ApplicationsList/MainController.php
+++ app/Http/Controllers/ApplicationsList/MainController.php
@@ -8,53 +8,84 @@ use App\Http\Controllers\Controller;
 class MainController extends Controller
 {
     public function applicationsList(Request $request) {
+        $icons = [
+            'timetable' => [
+                'bg' => 'blue',
+                'icon' => url('img/app-list/timetable.png'),
+            ],
+            'score' => [
+                'bg' => 'red',
+                'icon' => url('img/app-list/score.png'),
+            ],
+            'exam' => [
+                'bg' => 'green',
+                'icon' => url('img/app-list/exam.png'),
+            ],
+            'freeroom' => [
+                'bg' => 'red',
+                'icon' => url('img/app-list/freeroom.png'),
+            ],
+            'student' => [
+                'bg' => 'purple',
+                'icon' => url('img/app-list/student.png'),
+            ],
+            'card' => [
+                'bg' => 'yellow',
+                'icon' => url('img/app-list/card.png'),
+            ],
+            'borrow' => [
+                'bg' => 'blue',
+                'icon' => url('img/app-list/borrow.png'),
+            ],
+        ];
         $applicationsList = [
             [
                 'title' => '课表查询',
                 'route' => '/pages/timetable/timetable',
-                'bg' => 'blue',
-                'icon' => url('img/app-list/timetable.png'),
+                'bg' => $icons['timetable']['bg'],
+                'icon' => $icons['timetable']['icon'],
                 'disabled' => false,
             ],
             [
                 'title' => '成绩查询',
                 'route' => '/pages/score/score',
-                'bg' => 'red',
-                'icon' => url('img/app-list/score.png'),
+                'bg' => $icons['score']['bg'],
+                'icon' => $icons['score']['icon'],
             ],
             [
                 'title' => '考试安排',
                 'route' => '/pages/exam/exam',
-                'bg' => 'green',
-                'icon' => url('img/app-list/exam.png'),
+                'bg' => $icons['exam']['bg'],
+                'icon' => $icons['exam']['icon'],
             ],
             [
                 'title' => '空教室',
                 'route' => '/pages/freeroom/freeroom',
-                'bg' => 'red',
-                'icon' => url('img/app-list/freeroom.png'),
+                'bg' => $icons['freeroom']['bg'],
+                'icon' => $icons['freeroom']['icon'],
             ],
             [
                 'title' => '学生查询',
                 'route' => '/pages/student/student',
-                'bg' => 'purple',
-                'icon' => url('img/app-list/student.png'),
+                'bg' => $icons['student']['bg'],
+                'icon' => $icons['student']['icon'],
             ],
             [
                 'title' => '一卡通',
                 'route' => '/pages/card/card',
-                'bg' => 'yellow',
-                'icon' => url('img/app-list/card.png'),
+                'bg' => $icons['card']['bg'],
+                'icon' => $icons['card']['icon'],
             ],
             [
                 'title' => '借阅信息',
                 'route' => '/pages/borrow/borrow',
-                'bg' => 'blue',
-                'icon' => url('img/app-list/borrow.png'),
+                'bg' => $icons['borrow']['bg'],
+                'icon' => $icons['borrow']['icon'],
             ],
         ];
 
         $res = [
+            'icons' => $icons,
             'app-list' => $applicationsList
         ];
         return RJM($res, 1, 'ok');
