09eca70f445f5ae5dffc54207a0315bc59a00d4e
cccRaim <chen.raim@qq.com>
2017-09-10 14:50:20 +0800

    git pull命令修正

--- app/Console/Commands/GitPull.php
+++ app/Console/Commands/GitPull.php
@@ -37,7 +37,8 @@ class GitPull extends Command
      */
     public function handle()
     {
-        exec('cd ..');
-        exec('git pull');
+        exec("cd ..\ngit fetch\ngit pull", $output);
+
+        echo nl2br(implode($output, "\n"));
     }
 }
