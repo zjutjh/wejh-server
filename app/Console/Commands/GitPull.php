1f0257735c0a403eda85100161074df079ed7bc0
cccRaim <chen.raim@qq.com>
2017-10-13 19:34:38 +0800

    钩子优化

--- app/Console/Commands/GitPull.php
+++ app/Console/Commands/GitPull.php
@@ -11,7 +11,7 @@ class GitPull extends Command
      *
      * @var string
      */
-    protected $signature = 'git:pull';
+    protected $signature = 'git:pull {ref}';
 
     /**
      * The console command description.
