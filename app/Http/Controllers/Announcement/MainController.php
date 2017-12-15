5b39e3d2a18e161fe0fce05e49a6a711f3248d5c
cccRaim <chen.raim@qq.com>
2017-12-15 21:55:22 +0800

    增加可复制公告

--- app/Http/Controllers/Announcement/MainController.php
+++ app/Http/Controllers/Announcement/MainController.php
@@ -11,11 +11,17 @@ class MainController extends Controller
         $title = env('ANNOUNCEMENT_TITLE', '公告');
         $content = env('ANNOUNCEMENT_CONTENT', '<p>有任何问题，请加QQ群:462530805</p>');
         $show = env('ANNOUNCEMENT_SHOW', true);
+        $footer = env('ANNOUNCEMENT_FOOTER', '');
+        $clipboard = env('ANNOUNCEMENT_CLIPBOARD', '');
+        $clipboardTip = env('ANNOUNCEMENT_CLIPBOARD_TIP', '');
         $res = [
             'id' => env('ANNOUNCEMENT_ID', 1),
             'show' => $show,
             'title' => $title,
-            'content' => $content
+            'content' => $content,
+            'footer' => $footer,
+            'clipboard' => $clipboard,
+            'clipboardTip' => $clipboardTip
         ];
         return RJM($res, 1, 'ok');
     }
