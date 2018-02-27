583aeb05b8a5b4e6191f3a17a20916718521c02e
cccRaim <chen.raim@qq.com>
2018-02-27 20:48:11 +0800

    增加判断

--- app/Http/Controllers/Wechat/NotificationController.php
+++ app/Http/Controllers/Wechat/NotificationController.php
@@ -13,6 +13,9 @@ class NotificationController extends Controller
         $templateId = '1l8gWm3O0IGbWIFlaCo9K5c7q-U_-2MFBE-7I1xY-Is'; // 模板消息id
         $url = $request->get('url');
         $data = $request->get('data');
+        if (!$userId || !$data) {
+            return RJS(null, -1, 'need openid and data');
+        }
         $job = new SendTemplateMessage($userId, $templateId, $url, $data);
         dispatch($job);
         return RJM(null, 200, 'success');
