e0ff3681d9f21f0ef110ca642aaa347897fc0efc
Bunny Wong <bunnnywong@gmail.com>
2020-12-27 01:06:59 +0800

    新增接口用于获取红点配置

---
+++ app/Services/BadgeService.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Services;
+
+class BadgeService {
+
+    public static function getBadges()
+    {
+        $badges = [
+            '/home/feedback',
+            '/index/freeroom/moganshan',
+            '/index/questionnaire'
+        ];
+        return [
+            'allBadges' => $badges
+        ];
+    }
+
+}
