5cb0c8f700fe2996383ba6fa1ea23624aa29e24e
Ethan Wong <e1hanW0ng@gmail.com>
2021-06-05 22:37:42 +0800

    修复考试安排时间字段问题

--- app/Models/Api.php
+++ app/Models/Api.php
@@ -934,6 +934,8 @@ class Api extends Model
 
             if($between < 0) {
                 $g['倒计时名'] = '已经过去' . -$between . '天';
+            } else if ($between === 0){
+                $g['倒计时名'] = '今天';
             } else {
                 $g['倒计时名'] = '还有' . $between . '天';
             }
@@ -1004,7 +1006,14 @@ class Api extends Model
             $g['教师'] = $value['教师'];
             $g['课程'] = $value['课程'];
             $g['日期'] = $value['日期'];
-            $g['时段'] = isset($value['时段']) ? $value['时段'].'（实际为准）' : '暂无考试时间';
+            $parg = '/(\d{4}-\d{2}-\d{2})\((\d{2}:\d{2}-\d{2}:\d{2})\)/';
+            if (preg_match_all($parg, $value['日期'], $matches)) {
+                $g['日期'] = $matches[1][0];
+                $g['时段'] = $matches[2][0];
+            } else {
+                $g['时段'] = $values['时段'];
+            }
+            $g['时段'] = isset($g['时段']) ? $g['时段'].'（实际为准）' : '暂无考试时间';
             $g['教室'] = $value['教室'];
             $g['座位号'] = isset($value['zwh']) ? $value['zwh'] : '暂无座位号';
             $g['考试方式'] = isset($value['ksfs']) ? $value['ksfs'] : '暂无考试方式';
